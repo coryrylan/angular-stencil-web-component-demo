a1e53d1fdcce31353dbcdc8f2c8aedeb6beb0f28
Cory Rylan <splintercode.cb@gmail.com>
2018-05-06 21:45:56 -0500

    chore: update to angular 6

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('angular-stencil-web-component-demo App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
